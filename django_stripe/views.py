b8d5f4f9e612d4d90cea5de03bf672e7b226e8e0
Ahmad <ahmadw.waliullah.khan@gmail.com>
2019-12-17 15:40:42 +0530

    card payment test

--- django_stripe/views.py
+++ django_stripe/views.py
@@ -7,11 +7,15 @@ def index(request):
     stripe.api_key = 'sk_test_mm3n8XcnrbkJjhJA9G7sLHg700wgNTrKSd'
     
 
-    stripe.PaymentIntent.create(
-      amount=599,
+    response = stripe.PaymentIntent.create(
+      amount=699,
       currency='usd',
       payment_method_types=['card'],
       receipt_email='ahmad@spiderorb.com',
     )
     
-    return HttpResponse("Stripe Integration")
+    print(response)
+    
+    data = "amount : {}, payment_method' : {}".format(response.amount, response.payment_method_types[0])
+        
+    return HttpResponse(data)
