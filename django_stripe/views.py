5f6e636405f99a38d17a09cf5d15e964a0d82c5a
Ahmad <ahmadw.waliullah.khan@gmail.com>
2019-12-17 18:57:46 +0530

    Implemented Product creation, plan creation, customer creation

--- django_stripe/views.py
+++ django_stripe/views.py
@@ -16,7 +16,8 @@ def index(request):
       type="service",
     )
     
-    print(outpost_product.id)
+    print("PRODUCT CREATED...")
+    print(outpost_product)
     
     # create a basic plan under outpost product
     basic_plan = stripe.Plan.create(
@@ -26,7 +27,8 @@ def index(request):
       nickname='Basic Plan',
       amount=799,
     )
-     
+    
+    print("RECURRING PLAN CREATED (BASIC PLAN)...") 
     print(basic_plan)
     
     # create an executive plan under outpost product 
@@ -37,7 +39,63 @@ def index(request):
       nickname='Executive Plan',
       amount=1199,
     )
-     
+    
+    print("RECURRING PLAN CREATED (EXECUTIVE PLAN)...")  
     print(executive_plan)
-          
-    return HttpResponse("Stripe api ...")
+    
+    # creates a payment method
+    payment_method_response = stripe.PaymentMethod.create(
+      type="card",
+      card={
+        "number": dummy_card,
+        "exp_month": 12,
+        "exp_year": 2020,
+        "cvc": "314",
+      },
+    )
+    
+    print("PAYMENT METHOD (CARD) CREATED...") 
+    print(payment_method_response)
+     
+    
+    # create a customer
+    customer = stripe.Customer.create(
+      description="Test Customer through python",
+      email="jonsnow@crows.com",
+      name="Jon Snow",
+      address="Chandigarh, India, 781028",
+    )
+    
+    print("CUSTOMER CREATD...") 
+    print(customer)
+    
+     # attach the payment method to the customer 
+    attached_payment_method = stripe.PaymentMethod.attach(
+      payment_method_response.id,
+      customer=customer.id,
+    )
+    
+    print("ATTACHED PAYMENT METHOD...")
+    print(attached_payment_method)
+    
+    # make the payment method default one
+    default_payment_method = stripe.Customer.modify(
+      customer.id,
+      default_source = payment_method_response.id,
+    )
+    
+    print("CREATED DEFAULT PAYMENT METHOD...")
+    print(default_payment_method)
+    
+    # subscribe to outpost basic plan (recurring)
+    subscription = stripe.Subscription.create(
+      customer=customer.id,
+      default_payment_method=payment_method_response.id, 
+      items=[{"plan": basic_plan.id}],
+    )
+    
+    print("SUBSCRIPTION CREATED...")  
+    print(subscription)
+    
+            
+    return HttpResponse("Stripe payment subscription...")
