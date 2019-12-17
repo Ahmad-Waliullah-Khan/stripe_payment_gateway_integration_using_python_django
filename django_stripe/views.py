05927c25da6c4b77a528a6f25ca92e0bb44f7e72
Ahmad <ahmadw.waliullah.khan@gmail.com>
2019-12-17 17:47:06 +0530

    Created Stripe Product and plans under the product

--- django_stripe/views.py
+++ django_stripe/views.py
@@ -1,21 +1,43 @@
 from django.http import HttpResponse
 import stripe
-
+import json
 
 def index(request):
-
-    stripe.api_key = 'sk_test_mm3n8XcnrbkJjhJA9G7sLHg700wgNTrKSd'
     
-
-    response = stripe.PaymentIntent.create(
-      amount=699,
-      currency='usd',
-      payment_method_types=['card'],
-      receipt_email='ahmad@spiderorb.com',
+    client_secrect_key = 'sk_test_mm3n8XcnrbkJjhJA9G7sLHg700wgNTrKSd'
+    stripe.api_key = client_secrect_key
+    dummy_card = "4242424242424242"
+    
+    outpost_monthly_basic_plan = "plan_GNNxTEfOmkHe19"
+    
+    # Create a product 
+    outpost_product = stripe.Product.create(
+      name="OUTPOST Subscription",
+      type="service",
     )
     
-    print(response)
+    print(outpost_product.id)
     
-    data = "amount : {}, payment_method' : {}".format(response.amount, response.payment_method_types[0])
-        
-    return HttpResponse(data)
+    # create a basic plan under outpost product
+    basic_plan = stripe.Plan.create(
+      currency='usd',
+      interval='month',
+      product=outpost_product.id,
+      nickname='Basic Plan',
+      amount=799,
+    )
+     
+    print(basic_plan)
+    
+    # create an executive plan under outpost product 
+    executive_plan = stripe.Plan.create(
+      currency='usd',
+      interval='month',
+      product=outpost_product["id"],
+      nickname='Executive Plan',
+      amount=1199,
+    )
+     
+    print(executive_plan)
+          
+    return HttpResponse("Stripe api ...")
